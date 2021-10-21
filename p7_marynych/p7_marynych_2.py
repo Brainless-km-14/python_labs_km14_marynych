6e631f6da450996a0b01a4bb3b4293ff73c8fa74
Kolia <marinich2003@gmail.com>
2021-10-21 14:08:23 +0300

    p7_marynych

---
+++ p7_marynych/p7_marynych_2.py
@@ -0,0 +1,26 @@
+rgb = '#'
+def  main(a):
+    a = int(a)
+    numbers = []
+    for i in range(0,10):
+        numbers.append(i)
+    numbers += 'A', 'B', 'C', 'D', 'E', 'F'
+    c = str(numbers[a//16])
+    c += str(numbers[a%16])
+    return c
+def check(a):
+    if a.isdigit():
+        if 0 <= int(a) <= 255:
+            return True
+    print("Число не в інтревалі від 0 до 255!!!")
+    return False
+enter = "Введіть {} чилсло в інтервалі від 0 до 255: "
+
+for i in ("перше","друге","третє"):
+    while True:
+        num = input(enter.format(i))
+        if check(num):
+            rgb += main(num).zfill(2)
+            break
+print('The hexa color: '+rgb)
+print(rgb)
