e378f51d4aa16b9e5a7e77154aa6872849b98b39
Brainless-km-14 <90906546+Brainless-km-14@users.noreply.github.com>
2021-10-25 17:57:12 +0300

    Update p7_marynych_2.py

--- p7_marynych/p7_marynych_2.py
+++ p7_marynych/p7_marynych_2.py
@@ -1,14 +1,12 @@
 rgb = '#'
 numbers = [0,1,2,3,4,5,6,7,8,9,'A', 'B', 'C', 'D', 'E', 'F']
 enter = "Введіть {} чилсло в інтервалі від 0 до 255: "
-
 for i in ("перше", "друге", "третє"):
     while True:
         num = input(enter.format(i))
         if num.isdigit():
             if 0 <= int(num) <= 255:
-                answ = str(numbers[int(num) // 16])
-                answ += str(numbers[int(num) % 16])
+                answ = str(numbers[int(num) // 16]) + str(numbers[int(num) % 16])
                 rgb += answ.zfill(2)
                 break
             else:
