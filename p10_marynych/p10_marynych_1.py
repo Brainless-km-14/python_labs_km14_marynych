fadd2b69c1c4006b4a730bfdecafd3c5054a1f98
Kolia <marinich2003@gmail.com>
2021-11-08 17:21:09 +0200

    p10_marynych

--- p10_marynych/p10_marynych_1.py
+++ p10_marynych/p10_marynych_1.py
@@ -1,34 +1,10 @@
-import numpy as np
-month = input("Enter the month: ")
-while True:
-    if len(month)>2 or len(month)<1 or month.isdigit() == False or int(month)>12 or int(month)<1:
-                print("You entered the wrong month, please try again")
-                month = input("Enter the month: ")
-    else:
-        break
-year = input("Enter the year: ")
-while True:
-    if len(year)!=4 or month.isdigit() == False or int(year)>2023 or int(year)<1900:
-                print("You entered the wrong year, please try again")
-                year = input("Enter the year: ")
-    else:
-        break
-years = np.arange(1900, 2020+3, 1)
-#print(years)
-days= [31,29,31,30,31,30,31,31,30,31,30,31]
-def a(years):
-    l  = list(filter(lambda i : i%400 ==0,years))
-    l2 = list(filter(lambda i : i%400 !=0,years))
-    l3 = list(filter(lambda i : i%100 !=0,l2))
-    l4 = list(filter(lambda i : i%4 ==0,l3))
-    l +=l4
-    return l
+salary_list = [7.3, 8.5, 11, 12.7, 15.2, 21.12, 27.35]
+indexation = []
+new_salary = list(map(lambda i : (float(i)*130)/100,salary_list))
+new_salary = list(map(lambda i: round(i,2),new_salary))
+indexation = list(map(lambda i,j: float(i)-float(j),new_salary,salary_list))
+indexation = list(map(lambda i: round(i,2),indexation))
+print("Salary table:")
+for i in range (0,7):
+    print(salary_list[i] ,new_salary[i] ,indexation[i])
 
-def b(x,y,function):
-    if int(y) in function(years):
-        days[1]= 28
-    elif int(y) not in function(years):
-        days[1]=29
-    print("In the year",year,"in month",month,days[int(x)-1],"days")
-b(month,year,a)
-    
